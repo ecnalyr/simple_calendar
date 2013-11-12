2053a05776e3c72967f86573c006da20c499c6d3
Jason Rylance <ecnalyr@gmail.com>
2013-11-12 10:03:55 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SimpleCalendar::Application.config.secret_key_base = '1210faeb8a6623e94ec0529a4b1abbca58907b6626275f8b70f8438d7de6212e625ef01abce915a87bf2fbdb2ced86b18633ac7b28a1d2101d538d55f4de5379'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SimpleCalendar::Application.config.session_store :cookie_store, key: '_simple_calendar_session'
